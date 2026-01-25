705f95cd63717c4fdebea7492b53182bbfc7943a
iamgiridharan <giridharan8341@gmail.com>
2026-01-25 11:12:42 +0530

    update code

---
+++ Week3/One.java
@@ -0,0 +1,9 @@
+package Week3;
+
+public class One {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ Week3/Two.java
@@ -0,0 +1,11 @@
+package Week3;
+
+public class Two {
+
+    public static void main(String[] args) {
+
+        
+        
+    }
+    
+}
