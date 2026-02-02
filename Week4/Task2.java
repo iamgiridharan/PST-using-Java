abab3c1b6e2b5aa179702d7777839237f4a835ad
iamgiridharan <giridharan8341@gmail.com>
2026-02-02 22:51:10 +0530

    update

--- Week4/Task2.java
+++ Week4/Task2.java
@@ -7,23 +7,15 @@ class Task2 {
         int mid = n / 2;
 
         int[] count = new int[26];
-
-        // Count left half
         for (int i = 0; i < mid; i++) {
             char c = s.charAt(i);
             count[c - 'a']++;
         }
-
-        // Decide start of right half
         int start = (n % 2 == 0) ? mid : mid + 1;
-
-        // Subtract using right half
         for (int i = start; i < n; i++) {
             char c = s.charAt(i);
             count[c - 'a']--;
         }
-
-        // Check all counts are zero
         for (int i = 0; i < 26; i++) {
             if (count[i] != 0) {
                 return false;
